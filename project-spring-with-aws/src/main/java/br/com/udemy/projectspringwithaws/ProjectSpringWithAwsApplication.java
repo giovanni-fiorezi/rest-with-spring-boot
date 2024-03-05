9548253e7b60e4dcedced97487e59ce440e88afd
giovanni.giovanelli <giovanni.giovanelli@hypeone.com>
2024-03-04 21:48:48 -0300

    Primeiro commit do curso de SPring Boot com Cloud

---
+++ project-spring-with-aws/src/main/java/br/com/udemy/projectspringwithaws/ProjectSpringWithAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.projectspringwithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringWithAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringWithAwsApplication.class, args);
+    }
+
+}
---
+++ project-spring-with-aws/src/test/java/br/com/udemy/projectspringwithaws/ProjectSpringWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.projectspringwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringWithAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
